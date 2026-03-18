4de31fa6888e66053e4a55ae38a149e2faba8e08
Raekwon-Onirele <contato.raekwon@gmail.com>
2026-03-18 10:59:38 -0300

    Css dashboard

--- src/hooks/useFetchDocument.jsx
+++ src/hooks/useFetchDocument.jsx
@@ -38,6 +38,12 @@ export const useFetchDocument = (docCollection, search = null, uid = null) => {
             where("tags", "array-contains", search),
             orderBy("createdAt", "desc"),
           );
+        } else if (uid) {
+          q = await query(
+            collectionRef,
+            where("uid", "==", uid),
+            orderBy("createdAt", "desc"),
+          );
         } else {
           q = await query(collectionRef, orderBy("createdAt", "desc"));
         }
@@ -63,7 +69,7 @@ export const useFetchDocument = (docCollection, search = null, uid = null) => {
     }
 
     loadData();
-  }, [docCollection, search, uid, cancelled, documents]);
+  }, [docCollection, search, uid, cancelled]);
 
   useEffect(() => {
     return () => setCancelled(true);
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,79 @@
-import styles from "./Dashboard.module.css"
+// import CSS
+import styles from "./Dashboard.module.css";
+
+// import react router
+import { Link } from "react-router";
+
+// import context
+import { useAuthValue } from "../../context/authContext";
+
+// import hooks
+import { useFetchDocument } from "../../hooks/useFetchDocument";
 
 const Dashboard = () => {
-  return (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-  )
-}
+  // definindo user do useAuth para validação
+  const { user } = useAuthValue();
+  const uid = user.id;
 
-export default Dashboard
+  // definindo posts do user
+  const { documents: posts, loading } = useFetchDocument("posts", null, uid);
+
+  // função para deletar documento
+  const deleteDocument = (id) => {};
+
+  // função loading 
+  if(loading) {
+    return <p> Carregando... </p>
+  }
+
+  return (
+    <div className={styles.dashboard}>
+      <h2>Dashboard</h2>
+      <p>Gerencie seus Posts</p>
+      
+      {posts && posts.length === 0 ? (
+        // Caso não chegue nenhum post
+        <div className={styles.noposts}>
+          <p>Não foram encontrados posts</p>
+          <Link to="/posts/create" className="btn">
+            Crie seu primeiro post
+          </Link>
+        </div>
+      ) : (
+        // Se chegar algum post
+        <>
+          <div className={styles.postHeader}>
+            <span>Título</span>
+            <span>Ações</span>
+          </div>
+          {posts &&
+            posts.map((post) => (
+              <div key={post.id} className={styles.postRow}> 
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">
+                    Ver
+                  </Link>
+                  <Link
+                    to={`/posts/edit/${post.id}`}
+                    className="btn btn-outline"
+                  >
+                    Editar
+                  </Link>
+
+                  <button
+                    onClick={() => deleteDocument(post.id)}
+                    className="btn btn-outline btn-danger"
+                  >
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Dashboard;
